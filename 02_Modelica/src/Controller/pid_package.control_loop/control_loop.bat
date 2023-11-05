cd37ee6faff1cb279d1877665989fc0ba6eeaf1e
Sam.Roggeman@Gmail.com <Sam.Roggeman@gmail.com>
2023-11-05 19:29:18 +0100

    a

--- 02_Modelica/src/Controller/pid_package.control_loop/control_loop.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-SET PATH=C:/Program Files/OpenModelica1.21.0-64bit/bin/;C:/Program Files/OpenModelica1.21.0-64bit/lib//omc;C:/Program Files/OpenModelica1.21.0-64bit/lib/;C:/Users/samro/AppData/Roaming/.openmodelica/binaries/Modelica;C:/Users/samro/AppData/Roaming/.openmodelica/libraries/Modelica 4.0.0+maint.om/Resources/Library/mingw64;C:/Users/samro/AppData/Roaming/.openmodelica/libraries/Modelica 4.0.0+maint.om/Resources/Library/win64;C:/Users/samro/AppData/Roaming/.openmodelica/libraries/Modelica 4.0.0+maint.om/Resources/Library;C:/Program Files/OpenModelica1.21.0-64bit/bin/;%PATH%;
-SET ERRORLEVEL=
-CALL "%CD%/control_loop.exe" %*
-SET RESULT=%ERRORLEVEL%
-
-EXIT /b %RESULT%
