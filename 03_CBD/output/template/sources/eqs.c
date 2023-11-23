bded107cd039f224a9f44c164d6d1ad704c3dded
MaksimKarnaukh <maksim.karnaukh@student.uantwerpen.be>
2023-11-23 14:52:36 +0100

    copy pasted output code

--- 03_CBD/output/template/sources/eqs.c
+++ 03_CBD/output/template/sources/eqs.c
@@ -1,21 +1,19 @@
-// "delta" is the time delta since the previous iteration
-
 _Constant390_OUT1 = 390;
 _Const20_OUT1 = 20;
 _Integrator_zero_OUT1 = 0;
 _Integrator_delta_t_OUT1 = delta;
-_Integrator_multDelta_OUT1 = _Integrator_multDelta_IN1 + _Integrator_multDelta_IN1 + _Integrator_multDelta_IN2;
+_Integrator_multDelta_OUT1 = _Integrator_multDelta_IN1 * _Integrator_multDelta_IN1 * _Integrator_multDelta_IN2;
 _Integrator_sumState_OUT1 = _Integrator_sumState_IN1 + _Integrator_sumState_IN1 + _Integrator_sumState_IN2;
 _Derrivator_delta_t_OUT1 = delta;
 _Const0_OUT1 = 0;
-_Derrivator_multIc_OUT1 = _Derrivator_multIc_IN1 + _Derrivator_multIc_IN1 + _Derrivator_multIc_IN2;
+_Derrivator_multIc_OUT1 = _Derrivator_multIc_IN1 * _Derrivator_multIc_IN1 * _Derrivator_multIc_IN2;
 _Derrivator_neg1_OUT1 = -_Derrivator_neg1_IN1;
 _Derrivator_sum1_OUT1 = _Derrivator_sum1_IN1 + _Derrivator_sum1_IN1 + _Derrivator_sum1_IN2;
 _Derrivator_neg2_OUT1 = -_Derrivator_neg2_IN1;
 _Derrivator_sum2_OUT1 = _Derrivator_sum2_IN1 + _Derrivator_sum2_IN1 + _Derrivator_sum2_IN2;
 _Derrivator_inv_OUT1 = 1/_Derrivator_inv_IN1;
-_Derrivator_mult_OUT1 = _Derrivator_mult_IN1 + _Derrivator_mult_IN1 + _Derrivator_mult_IN2;
-_Product_Kd_OUT1 = _Product_Kd_IN1 + _Product_Kd_IN1 + _Product_Kd_IN2;
-_Product_Ki_OUT1 = _Product_Ki_IN1 + _Product_Ki_IN1 + _Product_Ki_IN2;
-_Product_Kp_OUT1 = _Product_Kp_IN1 + _Product_Kp_IN1 + _Product_Kp_IN2 + _Product_Kp_IN3;
+_Derrivator_mult_OUT1 = _Derrivator_mult_IN1 * _Derrivator_mult_IN1 * _Derrivator_mult_IN2;
+_Product_Kd_OUT1 = _Product_Kd_IN1 * _Product_Kd_IN1 * _Product_Kd_IN2;
+_Product_Ki_OUT1 = _Product_Ki_IN1 * _Product_Ki_IN1 * _Product_Ki_IN2;
+_Product_Kp_OUT1 = _Product_Kp_IN1 * _Product_Kp_IN1 * _Product_Kp_IN2 * _Product_Kp_IN3;
 _Summation_OUT1 = _Summation_IN1 + _Summation_IN1 + _Summation_IN2 + _Summation_IN3;
