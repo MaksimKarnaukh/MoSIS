d8351ecd12f4c52d09f5a9a41b23a19d813089b2
MaksimKarnaukh <maksim.karnaukh@student.uantwerpen.be>
2023-12-06 13:14:02 +0100

    reach , cov, invariants

--- 04_PetriNet/main.py
+++ 04_PetriNet/main.py
@@ -3,9 +3,10 @@ import os
 import graphviz as gv
 
 
-def generate_graph(input_file, output_file, type):
-    os.system(f"python RC.py {input_file} {output_file} {type} -m -p")
+def generate_graph(input_file, output_file, type, extra_args=""):
+    os.system(f"python RC.py {input_file} {output_file} {type} -m {extra_args}")
     os.system(f"dot -Tpng -o ./output/{type}_graph.png {output_file}")
+
 
 input_file = "./Petrinets/Petrinet1.tapn"
 
@@ -15,17 +16,6 @@ if __name__ == '__main__':
     # file is optional and the 'type' is expected to be a prefix of either 'reachability' or
     # 'coverability' (case-insensitve)
 
-    generate_graph(input_file, "./output/cov.dot", "coverability")
+    generate_graph(input_file, "./output/reach.dot", "reachability", "-p -I 200")
 
-    # doesn't work cause infinite
-    # generate_graph(input, "./output/cov.dot", "coverability")
-
-    # don't uncomment this
-    # os.system(f"python RC.py {input_file} ./output/net.dot coverability -g")
-    # os.system(f"dot -Tpng -o ./output/graph.png net.dot")
-
-    # os.system(f"python RC.py {input_file} ./output/net.dot coverability -p")
-
-
-
-
+    generate_graph(input_file, "./output/cov.dot", "coverability", "-p")
