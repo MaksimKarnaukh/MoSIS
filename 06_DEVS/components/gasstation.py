8ef5b98aa5339ee03f1248e08ec6c7973b804fba
Maksim <Maksim.Karnaukh@student.uantwerpen.be>
2023-12-28 03:36:20 +0100

    Changes by samro

--- 06_DEVS/components/gasstation.py
+++ 06_DEVS/components/gasstation.py
@@ -35,13 +35,19 @@ class GasStationState(object):
 
 
     def carArrived(self, car:Car, delay_time):
+        """
+        car has arrived at the gas station with a delay time
+        Adds the car to the queue and sorts the queue by remaining delay time
+        :param car: The car that has arrived
+        :param delay_time: The delay time
+        """
         self.cars.append([delay_time, car])
         # Sort the cars by their delay time low to high
         self.cars.sort(key=lambda x: x[0])
 
     def decreaseCarDelays(self, elapsed_time):
         """
-        Handles the elapsed time of the car delays
+        decreases the delay of every car by elapsed_time
         :param elapsed_time: The elapsed time
         """
         for i in range(len(self.cars)):
@@ -49,6 +55,9 @@ class GasStationState(object):
             self.cars[i][0] -= elapsed_time
 
     def gasStationIsAvailable(self):
+        """
+        returns True if the GasStation is available, False otherwise (no car is currently being queried)
+        """
         return self.next_car is None
 
 class GasStation(AtomicDEVS):
@@ -56,22 +65,6 @@ class GasStation(AtomicDEVS):
     Represents the notion that some Cars need gas.
     It can store an infinite amount of Cars, who stay for a certain delay inside an internal queue.
     This component can be available (default) or unavailable, as described below.
-    """
-
-    """
-    TODO
-        in car_in port As soon as one is entered, it is given a delay time, 
-        sampled from a normal distribution with mean 10 minutes and standard deviation of 130 seconds. 
-        Cars are required to stay at least 2 minutes in the GasStation. 
-        When this delay has passed and when this component is available, a Query is sent over the Q_send port. 
-        
-        When a QueryAck is received, the GasStation waits for QueryAck's t_until_dep time before outputting the next Car over the car_out output. 
-        Only then, this component becomes available again. If the waiting time is infinite, the GasStation keeps polling until it becomes finite again. 
-        
-        Q_send: Sends a Query as soon as a Car has waited its delay. Next, this component becomes unavailable, preventing collisions on the RoadSegment after. 
-        
-        car_out: Outputs the Cars, with no_gas set back to false.      
-              
     """
     def __init__(self, name, observ_delay=0.1):
         """
@@ -97,22 +90,26 @@ class GasStation(AtomicDEVS):
         if self.state.gasStationIsAvailable():
             # return the delay of the first car
             if len(self.state.cars) > 0:
+                # return the delay of the first car if it is positive
+                # otherwise return 0.0
                 return max(0.0, self.state.cars[0][0])
+            # return infinity and wait for a car to arrive
             return INFINITY
+        # specified variable time
         return self.state.next_time
 
     def outputFnc(self):
-        # if self.state.next_car is None:
-        #     return {}
-        if self.state.query_state == QueryState.NOT_SENT and not self.state.gasStationIsAvailable():
+        # if the query is yet to be sent, send it
+        if self.state.query_state == QueryState.NOT_SENT:
             return {
                 self.Q_send: Query(self.state.next_car[1].ID),
             }
-
+        # if the query is sent and acknowledged, the car leaves
         elif self.state.query_state == QueryState.ACKNOWLEDGED:
             return {
                 self.car_out: self.state.next_car[1],
             }
+        # base case
         else:
             return {}
 
@@ -122,8 +119,9 @@ class GasStation(AtomicDEVS):
         # decrease the delay of every car by self.elapsed
         self.state.decreaseCarDelays(self.timeAdvance())
 
-        # if there is no next car and there are cars in the queue
+        # if there is no next car
         if self.state.gasStationIsAvailable():
+            # if there are cars in the queue
             if len(self.state.cars) > 0:
                 # if the car with the shortest delay is ready to enter the GasStation
                 if self.state.cars[0][0] <= 0.0:
@@ -133,10 +131,13 @@ class GasStation(AtomicDEVS):
         else:
             # if there is a next car and it is not queried yet
             if self.state.query_state == QueryState.NOT_SENT:
-                # send the query
+                # the query has been sent
                 self.state.query_state = QueryState.SENT
+                # wait for the QueryAck
                 self.state.next_time = INFINITY
+            # if the query has been sent and acknowledged
             if self.state.query_state == QueryState.ACKNOWLEDGED:
+                # the car has left and the query is available again
                 self.state.query_state = QueryState.AVAILABLE
                 self.state.next_car = None
 
@@ -160,10 +161,14 @@ class GasStation(AtomicDEVS):
             # When a QueryAck is received, the GasStation waits for QueryAck's t_until_dep time
             # before outputting the next Car over the car_out output.
             self.state.next_time = inputs[self.Q_rack].t_until_dep
+            # if the delay is infinite, the GasStation must poll the QueryAck at a certain interval
             if self.state.next_time == INFINITY:
+                # set the next time to the observ_delay
                 self.state.next_time = self.state.observ_delay
+                # set the query state to not sent, so that the query is sent again
                 self.state.query_state = QueryState.NOT_SENT
             else:
+                # the query has been acknowledged
                 self.state.query_state = QueryState.ACKNOWLEDGED
 
         return self.state
