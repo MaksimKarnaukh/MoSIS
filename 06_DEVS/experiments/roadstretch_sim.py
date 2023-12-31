293fbd3c26b958f7aef89828b581f21c85bb7d43
Maksim <Maksim.Karnaukh@student.uantwerpen.be>
2023-12-31 02:58:48 +0100

    roadstretch sim plot for both short and long

--- 06_DEVS/experiments/roadstretch_sim.py
+++ 06_DEVS/experiments/roadstretch_sim.py
@@ -3,24 +3,48 @@ from other.roadstretch import RoadStretch
 import matplotlib.pyplot as plt
 
 
-def plot_cars(travel_time_per_car):
-    # plot the travel time per car with the car ID on the x-axis and the travel time on the y-axis
-    plt.plot([c[0].ID for c in travel_time_per_car], [c[1] for c in travel_time_per_car], 'ro')
-    # use logarimtic scale on the y-axis
-    plt.yscale('log')
-    # use a grid on x and y axis
-    plt.grid(True)
-    # make sure the x-axis contains the car IDs of all cars
-    plt.xticks([c[0].ID for c in travel_time_per_car])
-    # print the actual y value next to the point, rounded to 2 decimals
-    for i in travel_time_per_car:
-        plt.text(i[0].ID, i[1], str(round(i[1], 2)))
+def plot_tt_short(travel_time_per_car):
+    """
+    Plots the travel time per car for the short simulation.
+    :param travel_time_per_car: (list)
+        list of tuples (car, travel_time)
+    """
+    cars = [f"{c[0].ID}" for c in travel_time_per_car]
+    travel_times = [c[1] for c in travel_time_per_car]
 
-    plt.xlabel('Car ID')
-    plt.ylabel('Travel time')
+    # Plotting
+    plt.bar(cars, travel_times, color='blue')
+    plt.xlabel('Car IDs')
+    plt.ylabel('Travel Time (seconds)')
+    plt.title('Travel Times of Cars')
     plt.show()
 
-def handle_statistics(model):
+
+def plot_tt_long(travel_time_per_car):
+    """
+    Plots the travel time per car for the long simulation.
+    :param travel_time_per_car: (list)
+        list of tuples (car, travel_time)
+    """
+    # plot the travel time per car with the car ID on the x-axis and the travel time on the y-axis
+    plt.scatter([c[0].ID for c in travel_time_per_car], [c[1] for c in travel_time_per_car], c='red', marker='o')
+    # use logarimtic scale on the y-axis
+    plt.yscale('log')
+    # make sure the x-axis contains the car IDs of all cars
+    plt.xticks([c[0].ID for c in travel_time_per_car if c[0].ID % 10 == 0])
+    # print the actual y value next to the point, rounded to 2 decimals
+    # for i in travel_time_per_car:
+    #     plt.text(i[0].ID, i[1], str(round(i[1], 2)))
+    # make grid
+    plt.grid(True)
+
+    plt.xlabel('Car IDs')
+    plt.ylabel('Travel Time (log of seconds)')
+    plt.title('Travel Times of Cars')
+    plt.show()
+
+
+def handle_statistics(model, length):
     """
     Handles the plotting/printing of statistics of the simulation.
     :param model:
@@ -34,18 +58,39 @@ def handle_statistics(model):
     number_of_cars = statistics["number_of_cars"]
     travel_time_per_car = statistics["travel_time_per_car"]
 
-    print("Simulation statistics:")
+    print(f"Simulation statistics for {length} simulation:")
 
     # number of crashes
-    print("The number of crashes is: " + str(model.getNumberCrashes()))
-    print("The total time is: " + str(total_time))
+    print("\t -The number of crashes is: " + str(model.getNumberCrashes()))
+    print("\t -The number of cars that passed the roadstretch is: " + str(number_of_cars))
+    print("\t -The total time spent by cars in the roadstretch is: " + str(total_time))
+    print("\t -The average time spent by a car in the roadstretch is: " + str(average_travel_time))
+    print("\n")
 
     # plot the travel time per car
-    plot_cars(travel_time_per_car)
+    if length == "short":
+        plot_tt_short(travel_time_per_car)
+    else:
+        plot_tt_long(travel_time_per_car)
 
 
+def roadstretch_sim(termination_time, length, L, v_max, IAT_min, IAT_max, v_pref_mu, v_pref_sigma, destinations, limit):
 
-def roadstretch_sim(termination_time, cars_limit):
+    model = RoadStretch("roadstretch", L, v_max, IAT_min, IAT_max, v_pref_mu, v_pref_sigma, destinations, limit)
+    sim = Simulator(model)
+    sim.setClassicDEVS()
+    sim.setTerminationTime(termination_time)
+    sim.setVerbose(f"./traces/roadstretch_{termination_time}.txt")
+    sim.simulate()
+
+    handle_statistics(model, length)
+
+
+def roadstretch_sim_short():
+    """
+    Simulates the roadstretch model for a short period of time.
+    Runs the simulation for 100 time units with a limit of 50 cars.
+    """
 
     # constants for the road stretch
     L = 5  # length of the road segments
@@ -57,22 +102,17 @@ def roadstretch_sim(termination_time, cars_limit):
     destinations = ["collector"]
     limit = 50
 
-    model = RoadStretch("roadstretch", L, v_max, IAT_min, IAT_max, v_pref_mu, v_pref_sigma, destinations, limit)
-    sim = Simulator(model)
-    sim.setClassicDEVS()
-    sim.setTerminationTime(termination_time)
-    sim.setVerbose(f"./traces/roadstretch_{termination_time}.txt")
-    sim.simulate()
-
-    handle_statistics(model)
-
-
-def roadstretch_sim_short():
-    """
-    Simulates the roadstretch model for a short period of time.
-    Runs the simulation for 10000 time units with a limit of 5 cars.
-    """
-    roadstretch_sim(100, 50)
+    roadstretch_sim(
+        100,
+        "short",
+        L, v_max,
+        IAT_min,
+        IAT_max,
+        v_pref_mu,
+        v_pref_sigma,
+        destinations,
+        limit
+    )
 
 
 def roadstretch_sim_long():
@@ -80,11 +120,30 @@ def roadstretch_sim_long():
     Simulates the roadstretch model for a long period of time.
     Runs the simulation for 10000 time units with a limit of 50 cars.
     """
-    roadstretch_sim(10000, 50)
+
+    # constants for the road stretch
+    L = 5  # length of the road segments
+    v_max = 30  # maximum allowed velocity
+    IAT_min = 5
+    IAT_max = 7
+    v_pref_mu = 30
+    v_pref_sigma = 10
+    destinations = ["collector"]
+    limit = 50
+
+    roadstretch_sim(
+        10000,
+        "long",
+        L, v_max,
+        IAT_min,
+        IAT_max,
+        v_pref_mu,
+        v_pref_sigma,
+        destinations,
+        limit
+    )
 
 
 if __name__ == '__main__':
-
     roadstretch_sim_short()
-
-    # roadstretch_sim_long()
+    roadstretch_sim_long()
