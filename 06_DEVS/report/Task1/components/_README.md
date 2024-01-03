807a6020e8493ef2cb668cd35b80f9a4d033cd8d
Maksim <Maksim.Karnaukh@student.uantwerpen.be>
2024-01-03 21:23:01 +0100

    readme's

--- 06_DEVS/report/Task1/components/_README.md
+++
@@ -1,1 +0,0 @@
-Python package, containing all components.
--- 06_DEVS/report/Task1/components/querystate.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-class QueryState(Enum):
-    """
-    The state of the Query.
-    """
-    AVAILABLE = 1
-    NOT_SENT = 2
-    SENT = 3
-    ACKNOWLEDGED = 4
